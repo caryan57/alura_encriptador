0711cfcad9f01b32628a9be5b8717f3475cd6048
Carlos Olivares <focus.olivares@gmail.com>
2022-05-05 13:11:50 -0500

    Cambio para permitir el uso de signos de ¡! en el validador

--- js/validator.js
+++ js/validator.js
@@ -1,6 +1,6 @@
 function validateText(data){
     //Validar solo en minúsculas, espacios permitidos y ñ. No para mayúsuculas ni otros carácteres especiales.
-    let regex = /^[a-z\u00f1\s\,\¿?]+$/ 
+    let regex = /^[a-z\u00f1\s\,\¿?\¡!]+$/ 
     
     if(regex.test(data)){
         return true;
